23a8e915375ce17048be1911ffc333f3ae3d7221
dhanush <51503598+Dhanush111@users.noreply.github.com>
2024-11-14 16:49:34 +0530

    Time: 11 ms (25.02%), Space: 45.4 MB (5.76%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,24 +1,32 @@
 class Solution {
     public String reverseWords(String s) {
-        String tans=trim(s);
-        String[] arr=tans.split(" ");
+        String tans=s.trim();
+        String[] arr=tans.split(" +");//" +"means all spaces between those words
         String ans="";
         for(int i=arr.length-1;i>0;i--){
             ans=ans+arr[i]+" ";
         }
         ans=ans+arr[0];
         return ans;
-    }
-    public String trim(String s){
-        String a=s.trim();
-        String b="";
-        for(int i=0;i<a.length()-1;i++){
-            if(a.charAt(i)!=' ' || a.charAt(i)==' ' && a.charAt(i+1)!=' ' ){
-               b=b+a.charAt(i);
-            }
+    //     String tans=trim(s);
+    //     String[] arr=tans.split(" ");
+    //     String ans="";
+    //     for(int i=arr.length-1;i>0;i--){
+    //         ans=ans+arr[i]+" ";
+    //     }
+    //     ans=ans+arr[0];
+    //     return ans;
+    // }
+    // public String trim(String s){
+    //     String a=s.trim();
+    //     String b="";
+    //     for(int i=0;i<a.length()-1;i++){
+    //         if(a.charAt(i)!=' ' || a.charAt(i)==' ' && a.charAt(i+1)!=' ' ){
+    //            b=b+a.charAt(i);
+    //         }
             
-        }
-        b=b+a.charAt(a.length()-1);
-        return b;
+    //     }
+    //     b=b+a.charAt(a.length()-1);
+    //     return b;
     }
 }
