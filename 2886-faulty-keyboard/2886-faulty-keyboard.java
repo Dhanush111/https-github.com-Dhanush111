f6ee32f42fb1bb7649951a06021030b06c05c9be
dhanush <51503598+Dhanush111@users.noreply.github.com>
2024-11-28 16:51:43 +0530

    Time: 3 ms (97.46%), Space: 45.1 MB (24.09%) - LeetHub

---
+++ 2886-faulty-keyboard/2886-faulty-keyboard.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String finalString(String s) {
+       StringBuilder sb=new StringBuilder();
+       boolean found=false;
+       for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='i'){
+                found=!found;
+            }            
+            if(found==true && s.charAt(i)!='i'){
+                sb.insert(sb.length(),s.charAt(i));
+            }
+            else if(s.charAt(i)!='i'){
+                sb.insert(0,s.charAt(i));
+            }
+       } 
+       if(!found){
+        return sb.reverse().toString();
+       }
+       return sb.toString();
+    }
+}
